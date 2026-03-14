f32927d48ff54594d153b2d206f00ac0af53e879
eren sen <erensen@eren-MacBook-Air.local>
2026-03-14 23:07:22 +0300

    Relax Vercel adapter Express typing

--- api/[[...route]].ts
+++ api/[[...route]].ts
@@ -7,5 +7,5 @@ export default async function handler(
   res: ServerResponse,
 ): Promise<void> {
   await ensureAppReady();
-  app.handle(req as never, res as never, () => undefined);
+  (app as any).handle(req, res, () => undefined);
 }
